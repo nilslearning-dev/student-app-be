59201e66c5ec19d5f665372d6ba4bddd6bd31a53
nilslearning-dev <nilslearning987@gmail.com>
2026-08-04 19:20:46 +0530

    cors issue

--- src/main/java/com/example/studentregistrationapplication/controller/StudentController.java
+++ src/main/java/com/example/studentregistrationapplication/controller/StudentController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/students")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class StudentController {
 
     @Autowired
